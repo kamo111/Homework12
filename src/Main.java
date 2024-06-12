885bea279def6397f4d52deb2d7fc8525912fecc
kamo111 <kamo19.989king@gmail.com>
2024-06-12 21:24:14 +0300

    HW-12 done fix bugs

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
         Author ivan = new Author("Иван", "Иванович");
         Author andrey = new Author("Андрей", "Андреевич");
 
-
         Book book1 = new Book("Война и мир", ivan, 1998);
         System.out.println("Название = " + book1.getName());
         System.out.println("Автор = " + book1.getAuthor());
@@ -17,6 +16,6 @@ public class Main {
         System.out.println("Название = " + book2.getName());
         System.out.println("Автор = " + book2.getAuthor());
         System.out.println("Год издания = " + book2.getYear());
-
+        System.lineSeparator();
     }
 }
